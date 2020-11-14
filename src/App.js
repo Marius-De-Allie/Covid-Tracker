eb3f284d9649b60af1cec98fb952846fc5f09c6b
Marius-De-Allie <spectre4300s@yahoo.com>
2020-11-13 22:36:40 -0500

    Call getGlobalTimeLine function within App component's useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import React, { useEffect } from 'react';
+import getGlobalTimeline from './utils/api';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    getGlobalTimeline()
+      .then(data => {
+        console.log(data.data[0]);
+      })
+
+  });
   return (
     <div className="App">
       App
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 const getGlobalTimeline = () => {
-  fetch('https://corona-api.com/timeline')
+  return fetch('https://corona-api.com/timeline')
     .then(res => res.json())
     .catch(e => console.warn(e));
 };
 
-export { getGlobalTimeline };
+export { getGlobalTimeline as default };
