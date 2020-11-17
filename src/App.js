d77666d9c1ee6415011d6717f5494323601f36c4
Marius-De-Allie <spectre4300s@yahoo.com>
2020-11-17 18:31:52 -0500

    Render Dashboard component with App component, add globalStats prop to Dashboard component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Dashboard from './components/Dashboard';
 import getGlobalTimeline from './utils/api';
 import './App.css';
 
@@ -17,25 +18,7 @@ function App() {
   }, []);
   return console.log(globalStats) || (
     <div className="App">
-      <h1>Global Totals</h1>
-      <p>Refresh all</p>
-      {!globalStats 
-        ? <p>Loading...</p> 
-        : 
-        (
-          <React.Fragment>
-            <div style={{border: 'solid 1px black'}}>
-              <h2>Total Confirmed cases</h2>
-              <h2 style={{color: 'dodgerblue'}}>{`${(globalStats.confirmed / 1000000).toFixed(2)} M`}</h2>
-              Icon PlaceHolder
-              <p style={{cursor: 'pointer'}}>refresh</p>
-            </div>
-            <p>{`Total Confirmed deaths: ${globalStats.deaths}`}</p>
-            <p>{`Recovered: ${globalStats.recovered}`}</p>
-            <p>{`Updated at: ${new Date(globalStats.updated_at).toLocaleString()}`}</p>
-          </React.Fragment>
-        )
-      }
+      <Dashboard globalStats={globalStats} />
     </div>
   );
 }
