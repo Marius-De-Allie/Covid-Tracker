516276811f4f996bcb55e67629caa188044825ec
Marius-De-Allie <spectre4300s@yahoo.com>
2020-11-17 21:33:41 -0500

    Update rendered content within Dashboard component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,20 +6,36 @@ const Dashboard = ({ globalStats }) => {
     <React.Fragment>
       <h1>Global Totals</h1>
       <p>Refresh all</p>
+      {globalStats && <p>{`Last updated on: ${new Date(globalStats.updated_at).toLocaleString()}`}</p>}
       {!globalStats 
         ? <p>Loading...</p> 
         : 
         (
           <React.Fragment>
-            <div style={{border: 'solid 1px black'}}>
+            <div className='dash-card' style={{border: 'solid 1px dodgerblue', marginBottom: '5px'}}>
               <h2>Total Confirmed cases</h2>
               <h2 style={{color: 'dodgerblue'}}>{`${(globalStats.confirmed / 1000000).toFixed(2)} M`}</h2>
               Icon PlaceHolder
               <p style={{cursor: 'pointer'}}>refresh</p>
             </div>
-            <p>{`Total Confirmed deaths: ${globalStats.deaths}`}</p>
-            <p>{`Recovered: ${globalStats.recovered}`}</p>
-            <p>{`Updated at: ${new Date(globalStats.updated_at).toLocaleString()}`}</p>
+            <div className='dash-card' style={{border: 'solid 1px orange', marginBottom: '5px'}}>
+              <h2>Total Active cases</h2>
+              <h2 style={{color: 'orange'}}>{`${(globalStats.active / 1000000).toFixed(2)} M`}</h2>
+              Icon PlaceHolder
+              <p style={{cursor: 'pointer'}}>refresh</p>
+            </div>
+            <div className="dash-card" style={{border: 'solid 1px red', marginBottom: '5px'}}>
+              <h2>Total Confirmed deaths</h2>
+              <h2 style={{color: 'red'}}>{`${(globalStats.deaths / 1000000).toFixed(2)} M`}</h2>
+              Icon PlaceHolder
+              <p style={{cursor: 'pointer'}}>refresh</p>
+            </div>
+            <div className="dash-card" style={{border: 'solid 1px green',  marginBottom: '5px'}}>
+              <h2>Total Recovered cases</h2>
+              <h2 style={{color: 'green'}}>{`${(globalStats.recovered / 1000000).toFixed(2)} M`}</h2>
+              Icon PlaceHolder
+              <p style={{cursor: 'pointer'}}>refresh</p>
+            </div>
           </React.Fragment>
         )
       }
